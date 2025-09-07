26294f027816a51a6c73b3e5bd70d809b956bad0
Jacob Fliss <newgmobile@gmail.com>
2025-09-06 20:45:24 -0400

    overlay size adjustments

--- Controller/Classes/GameSettings.cs
+++ Controller/Classes/GameSettings.cs
@@ -27,16 +27,16 @@ public class GameSettings
     }
     public class StarCraft1
     {
-        public static readonly int overlayWidth = 280;
-        public static readonly int overlayHeight = 255;
+        public static readonly int overlayWidth = 340;
+        public static readonly int overlayHeight = 240;
         public static readonly int cellColumns = 4;
         public static readonly int sideOffset = 5;
         public static readonly int bottomOffset = 10;
     }
     public class StarCraft2
     {
-        public static readonly int overlayWidth = 320;
-        public static readonly int overlayHeight = 220;
+        public static readonly int overlayWidth = 300;
+        public static readonly int overlayHeight = 200;
         public static readonly int cellColumns = 6;
         public static readonly int sideOffset = 20;
         public static readonly int bottomOffset = 10;
